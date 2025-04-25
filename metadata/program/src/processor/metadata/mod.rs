c1b71d8c3e1e330f36d49c17b58baff7ea7005c3
Philip James <philip@btigroup.ee>
2025-04-25 10:05:30 +0300

    fix create_metadata_accounts file name

--- metadata/program/src/processor/metadata/mod.rs
+++ metadata/program/src/processor/metadata/mod.rs
@@ -1,5 +1,5 @@
-mod create_medatata_accounts;
+mod create_metadata_accounts;
 mod update_metadata_accounts;
 
-pub use create_medatata_accounts::*;
+pub use create_metadata_accounts::*;
 pub use update_metadata_accounts::*;
